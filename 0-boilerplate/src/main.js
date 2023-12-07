bb4407967fce0fcb4cebb54b0c1a3ecf051863df
soyoon26 <barcardi26@gmail.com>
2023-12-07 23:48:51 +0900

    환경 구성

---
+++ 0-boilerplate/src/main.js
@@ -0,0 +1,11 @@
+import * as THREE from "three";
+//three 설치하고 불러오기
+
+window.addEventListener("load", function () {
+  init();
+});
+//load가 되면 init실행
+
+function init() {
+  console.log(THREE);
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+npm create vite
+Vanilla JavaScript 선택
+npm install
+npm run dev
+npm install three
+
+vite.config.js로 root가 src라고 알려주기
