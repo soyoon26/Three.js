2cf8f3dff72bc28e0ca8035a08518f952dcc1fa5
soyoon26 <barcardi26@gmail.com>
2023-12-14 21:25:42 +0900

    다면체 변경

--- cube/src/main.js
+++ cube/src/main.js
@@ -25,9 +25,12 @@ function init() {
     500 //far
   ); //카메라 추가
 
-  const geometry = new THREE.BoxGeometry(2, 2, 2); //높이, 넓이, 길이
-  const material = new THREE.MeshStandardMaterial({
+  //const geometry = new THREE.BoxGeometry(2, 2, 2); //높이, 넓이, 길이
+  const cubeGeometry = new THREE.IcosahedronGeometry(1); //반지름 길이
+  //radius, detail
+  const cubeMaterial = new THREE.MeshLambertMaterial({
     color: new THREE.Color(0xff8b94), //0xff8b94, '#시작','색이름'
+    emissive: 0x111111,
     //transparent: true,
     //opacity: 0.5, transparent가 flase면 영향x
     //visible: flase,
@@ -36,31 +39,46 @@ function init() {
   }); //MeshBasicMaterial은 조명에 W영향받지 않음
 
   //material.color = new THREE.Color(0xff8b94) 로도 가능
+  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  const skeletonGeometry = new THREE.IcosahedronGeometry(2); //반지름 길이
+  const skeletonMaterial = new THREE.MeshBasicMaterial({
+    wireframe: true,
+    transparent: true,
+    opacity: 0.2,
+    color: 0xff5675,
+  });
+  const skeleton = new THREE.Mesh(skeletonGeometry, skeletonMaterial);
+  scene.add(cube, skeleton);
+  //scene.add(skeleton);
 
-  // camera.position.z=5
-  camera.position.set(3, 4, 5); //앞방향으로 이동
-  camera.lookAt(cube.position);
-  const directionalLight = new THREE.DirectionalLight(0xf0f0f0, 1); //조명의 색과 강도
+  camera.position.z = 5;
+  //camera.position.set(3, 4, 5); //앞방향으로 이동
+  //camera.lookAt(cube.position);
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 1); //조명의 색과 강도
 
-  directionalLight.position.set(-1, 2, 3);
+  //directionalLight.position.set(-1, 2, 3);
 
   scene.add(directionalLight);
 
-  const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
+  //const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
 
-  ambientLight.position.set(3, 2, 1);
-  scene.add(ambientLight);
+  //ambientLight.position.set(3, 2, 1);
+  //scene.add(ambientLight);
   const clock = new THREE.Clock();
   render();
   function render() {
+    const elapsedTime = clock.getElapsedTime();
     //cube.rotation.x = THREE.MathUtils.degToRad(45); 라디안으로 적용됨
-    cube.rotation.x += clock.getDelta();
+    cube.rotation.x = elapsedTime;
+    cube.rotation.y = elapsedTime;
+    //cube.rotation.x += clock.getDelta();
     //= clock.getElapsedTime();
     //Date.now() / 1000;
     //빌트인으로 제공하는 클락을 사용해도 됨
+
+    skeleton.rotation.x = elapsedTime * 1.5;
+    skeleton.rotation.y = elapsedTime * 1.5;
 
     //cube.position.y = Math.sin(cube.rotation.x); //1에서 -1사이
     //cube.scale.x = Math.cos(cube.rotation.x);
